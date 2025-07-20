37dc4904843bd25f37f0af98db8514a2f15d176f
Jonathan McKinnon <150212666+JohnnyMcKinnon@users.noreply.github.com>
2025-07-20 23:22:19 +0000

    completed methods contain and rehash code. Got a pass during the test code.

--- HashTable.java
+++ HashTable.java
@@ -1,5 +1,5 @@
 /**
- * A simple hasht table is an array of linked lists. In its simplest form, a
+ * A simple hash table is an array of linked lists. In its simplest form, a
  * linked list is represented by its first node. Typically we label this node as
  * "head". Here, however, we'll know it's the first node of the list because it
  * will be placed in an array element. For example, if we have 4 linked lists,
@@ -44,7 +44,7 @@ public class HashTable<E extends Comparable<E>> {
         this.loadFactor = 0.0;
     } // basic constructor
 
-    /** Default constructor, passes defauilt size to basic constructor */
+    /** Default constructor, passes default size to basic constructor */
     public HashTable() {
         this(DEFAULT_SIZE);
     } // default constructor
@@ -56,26 +56,22 @@ public class HashTable<E extends Comparable<E>> {
      * @param content E The content of a new node, to be placed in the array.
      */
     public void add(E content) {
-        // Create the new node to add to the hashtable
-        Node<E> newNode = new Node<E>(content);
-        // Use the hashcode for the new node's contents to find where to place it in the
-        // underlying array. Use abs in case hashcode < 0.
-        int position = Math.abs(content.hashCode()) % this.underlying.length;
-        // Check if selected position is already in use
-        if (this.underlying[position] == null) {
-            // Selected position not in use. Place the new node here and update the usage of
-            // the underlying array.
-            this.underlying[position] = newNode;
-            this.usage += 1;
-        } else {
-            // Selected position in use. We will append its contents to the new node first,
-            // then place the new node in the selected position. Effectively the new node
-            // becomes the first node of the existing linked list in this position.
-            newNode.setNext(this.underlying[position]);
-            this.underlying[position] = newNode;
-        }
-        // Update the number of nodes
-        this.totalNodes += 1;
+//calculates the array index(LL) for the new Node
+       int position = Math.abs(content.hashCode()) % this.underlying.length;
+       Node<E> newNode = new Node<>(content);
+//tests for the specific index of the content to see if it is empty or not(null)
+       if(this.underlying[position] == null) {
+        this.underlying[position] = newNode;
+        this.usage++;
+       }
+//if it is not empty, the node is inserted at the front of the LL
+       else{
+        newNode.setNext(this.underlying[position]);
+        this.underlying[position] = newNode;
+       }
+//Housekeeping code to keep track of total node number and maintain hash size and determines when to rehash
+       this.totalNodes++;
+       this.loadFactor = (double) this.usage / this.underlying.length;
     } // method add
 
     /**
@@ -85,14 +81,56 @@ public class HashTable<E extends Comparable<E>> {
      * hashcode. The linked list must then be traversed to determine if a node with
      * similar content and the target value is present or not.
      * 
-     * @param target E value to searc for
+     * @param target E value to search for
      * @return true if target value is present in one of the linked lists of the
      *         underlying array; false otherwise.
      */
     public boolean contains(E target) {
-        return false;
+//calculates array index
+        int position = Math.abs(target.hashCode()) % this.underlying.length;
+        Node<E> current = this.underlying[position];
+        //sets current to first element in this index array
+            while(current != null){
+                if(current.getContent().equals(target)){
+                    return true;
+//iterates through the LL, comparing each index with the target, and returns true if there is a match
+                }
+//iterates through the nodes in the LL
+                current = current.getNext();
+            }
+            return false;
     } // method contains
 
+    private void rehash(){
+//doubles initial hash size by 2 and resets usage and totalNode to 0 for an accurate recount when redistributing
+        Node<E>[] newArray = new Node[this.underlying.length * 2];
+        this.usage = 0;
+        this.totalNodes = 0;
+//loops over each index in old array
+        for(int i = 0; i < this.underlying.length; i++){
+            Node<E> current = this.underlying[i];
+//runs through each node in the LL
+            while (current != null){
+                Node<E> nextNode = current.getNext();
+//recomputes hashcode based on the new length of the LL
+                int newPos = Math.abs(current.getContent().hashCode()) % newArray.length;
+                if (newArray[newPos] == null){
+                    this.usage++;
+//if spot is empty, increase the count of non-null LL
+                }
+//give new element the first spot in the LL
+                current.setNext(newArray[newPos]);
+                newArray[newPos] = current;
+//increment count for the new node addition 
+                this.totalNodes++; 
+                current = nextNode; 
+            }
+        }
+//sets internal array to the new and resized array and update loadFactor 
+        this.underlying = newArray;
+        this.loadFactor = (double) this.usage / this.underlying.length;
+    }//Method rehash
+        
     /** Constants for toString */
     private static final String LINKED_LIST_HEADER = "\n[ %2d ]: ";
     private static final String EMPTY_LIST_MESSAGE = "null";
@@ -100,23 +138,22 @@ public class HashTable<E extends Comparable<E>> {
     private static final String NODES_INFORMATION = "\nTotal number of nodes: %d";
     private static final String NODE_CONTENT = "%s --> ";
 
-    /** String representationf for the object */
+    /** String representation for the object */
     public String toString() {
         // Initialize the StringBuilder object with basic info
         StringBuilder sb = new StringBuilder(
-                String.format(ARRAY_INFORMATION,
-                        this.usage, this.underlying.length));
+                String.format(ARRAY_INFORMATION, this.usage, this.underlying.length));
         sb.append(String.format(NODES_INFORMATION, this.totalNodes));
         // Iterate the array
         for (int i = 0; i < underlying.length; i++) {
             sb.append(String.format(LINKED_LIST_HEADER, i));
-            Node head = this.underlying[i];
+            Node<E> head = this.underlying[i];
             if (head == null) {
                 // message that this position is empty
                 sb.append(EMPTY_LIST_MESSAGE);
             } else {
                 // traverse the linked list, displaying its elements
-                Node cursor = head;
+                Node<E> cursor = head;
                 while (cursor != null) {
                     // update sb
                     sb.append(String.format(NODE_CONTENT, cursor));
